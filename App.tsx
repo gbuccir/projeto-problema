56effeeaed41c11bcd3fe48fe00a9307ca220007
gbuccir <gbucci@uol.com.br>
2019-12-13 20:29:25 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ULA TUTUUUUUUUU</Text>
+      <Text>Vamos ver se funciona mesmo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    color:'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
