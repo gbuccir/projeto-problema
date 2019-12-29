e652810b019ded8a80b90d342f5c3e49a2d601e4
gbuccir <gbucci@uol.com.br>
2019-12-28 22:01:52 -0300

    camera nao funciona

--- android/app/src/main/java/com/projetoissue/MainActivity.java
+++ android/app/src/main/java/com/projetoissue/MainActivity.java
@@ -4,13 +4,21 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.facebook.react.modules.core.PermissionAwareActivity;
+import com.facebook.react.modules.core.PermissionListener;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity implements PermissionAwareActivity{
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
+    private @Nullable   Callback mPermissionsCallback;
+  private @Nullable PermissionListener mPermissionListener;
+  @Nullable
+  private PermissionListener permissionListener;
+
     @Override
     protected String getMainComponentName() {
         return "projetoIssue";
@@ -25,4 +33,24 @@ public class MainActivity extends ReactActivity {
             }
         };
     }
+
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M)
+    public void requestPermissions(String[] permissions, int requestCode, PermissionListener listener) {
+      mPermissionListener = listener;
+      requestPermissions(permissions, requestCode);
+    }
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+      mPermissionsCallback = new Callback() {
+        @Override
+        public void invoke(Object... args) {
+          if (mPermissionListener != null && mPermissionListener.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            mPermissionListener = null;
+          }
+        }
+      };
+    }
+
 }
--- src/components/novoProblema.js
+++ src/components/novoProblema.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Picker, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Picker, Alert, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import ManutencaoEnum from '../utils/enums'
+import { RNCamera } from 'react-native-camera';
+import { withNavigationFocus } from 'react-navigation'
+
+import * as Permissions from 'expo-permissions';
+// import {PermissionsAndroid} from 'react-native';
+
+
 
 
 export default class NovoProblema extends Component {
@@ -24,6 +31,23 @@ export default class NovoProblema extends Component {
     this.state.idUsuario = this.props.navigation.getParam('login');
   }
 
+
+  tiraFoto = () => {
+    RNCamera.Constants.CameraStatus = 'READY';
+    RNCamera.Constants.RecordAudioPermissionStatus = 'AUTHORIZED';
+    const granted = Permissions.askAsync(Permissions.WRITE_EXTERNAL_STORAGE);
+    this.render();
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      alert(data.uri);
+    }
+  }
+
+
   render() {
     const { navigation } = this.props;
     const usuario = navigation.getParam('login');
@@ -31,7 +55,7 @@ export default class NovoProblema extends Component {
     return (
       <View style={styles.container}>
         <Text>Escolha o CDA</Text>
-        <Picker placeholder="CDA selecionado" style={{height: 50, width: 100}} selectedValue={this.state.language} onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}>
+        <Picker placeholder="CDA selecionado" style={{ height: 50, width: 100 }} selectedValue={this.state.language} onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}>
           <Picker.Item label="Java" value="java" />
           <Picker.Item label="JavaScript" value="js" />
         </Picker>
@@ -43,10 +67,31 @@ export default class NovoProblema extends Component {
         <Button title="Preventiva" onPress={() => this.setTipoManutencao(ManutencaoEnum.Preventiva)} />
         <Button title="Corretiva" onPress={() => this.setTipoManutencao(ManutencaoEnum.Corretiva)} />
 
-        {/* <Text>Se quiser tire uma foto </Text> */}
+        <Text>Se quiser tire uma foto </Text>
+        <RNCamera
+          // ref={camera => { this.xamera = camera }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          autoFocus={RNCamera.Constants.AutoFocus.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
 
-        <Button title="Salvar" onPress={() => this.salvar()} />
-      </View>
+        />
+
+        {/* 
+androidCameraPermissionOptions={ title= "Permitir camera",
+            message= "deve permitir uso para foto",
+            buttonPositive= "ok",
+            buttonNegative= "cancelar"} */}
+
+
+        <View >
+          <TouchableOpacity onPress={this.takePicture} style={styles.capture}>
+            <Text style={styles.buttonText}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Button title="Salvar" onPress={() => this.tiraFoto()} />
+      </View >
     );
   };
 }
@@ -54,9 +99,33 @@ export default class NovoProblema extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    height: 500,
+    width: 500,
+  },
+  buttonContainer: {
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
+  },
+  buttonText: {
+    fontSize: 14
+  }
 });
